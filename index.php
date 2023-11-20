b638ff1c8c24ca17d699e657cac2571c3bc07bf9
Diki setiawan <dikisetia101@gmail.com>
2023-11-20 11:09:58 +0700

    update

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
     // menerapkan notifikasi menggunakan $_GET 
     $notif = isset($_GET["notif"]) ? $_GET["notif"] : false;
     $email = isset($_GET["email"]) ? $_GET["email"] : false;
+    $nama = isset($_GET["nama"]) ? $_GET["nama"] : false;
+    $alamat = isset($_GET["alamat"]) ? $_GET["alamat"] : false;
 
     ?>
     <title>Form Pendaftaran</title>
@@ -36,6 +38,10 @@
                             Selamat, Data berhasil ditambahkan!
                          <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                         </div>";
+                    echo "<script>
+                    alert('Pendaftaran berhasil, silahkan cek email anda.');
+                    
+                    </script>";
                 } elseif ($notif == "email") {
                     echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                             Maaf, email yang kamu inputkan sudah terdaftar.
@@ -49,18 +55,18 @@
                         <form action="<?php echo BASE_URL . "proses_pendaftaran.php"; ?>" method="POST">
                             <div class="element-input">
                                 <label for="">Nama</label>
-                                <span><input type="text" name="nama" class="form-control" required /></span>
+                                <span><input type="text" name="nama" class="form-control" value="<?php echo $nama; ?>" required /></span>
                             </div>
                             <div class="element-input">
                                 <label for="email">email</label>
-                                <span><input type="email" id="email" name="email" class="form-control" required /></span>
+                                <span><input type="email" id="email" name="email" class="form-control" value="<?php echo $email; ?>" required /></span>
                             </div>
                             <div class="element-input">
                                 <label for="">Alamat</label>
-                                <span><textarea name="alamat" cols="30" rows="10" class="form-control" required></textarea></span>
+                                <span><textarea name="alamat" cols="30" rows="10" class="form-control" required><?php echo $alamat; ?></textarea></span>
                             </div>
                             <div class="element-input">
-                                <input type="submit" value="Submit" class="btn btn-primary">
+                                <input type="submit" name="submit" value="Submit" class="btn btn-primary">
                             </div>
                         </form>
                     </div>
@@ -90,14 +96,14 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
+    <!-- 
+    < !--Option 2: Separate Popper and Bootstrap JS < !-- < script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"> -->
+    <!-- </script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+            -->
 </body>
 
 
--- proses_pendaftaran.php
+++ proses_pendaftaran.php
@@ -2,19 +2,46 @@
 include_once("function/helper.php");
 include_once("function/koneksi.php");
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+
+
 // menghindari SQL injection
 $nama = mysqli_real_escape_string($koneksi, $_POST["nama"]);
 $email = mysqli_real_escape_string($koneksi, $_POST["email"]);
 $alamat = mysqli_real_escape_string($koneksi, $_POST["alamat"]);
 
+if (isset($_POST['submit'])) {
 
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'senadasetia101@gmail.com';
+    $mail->Password = 'vnftmdsjgrvazoby';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    $mail->setFrom('senadasetia101@gmail.com');
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+    $mail->Subject = "Konfirmasi pendaftaran!";
+    $mail->Body = "HI, " . $nama . " Selamat Pendaftaran kamu telah berhasil!";
+
+    $mail->send();
+}
 $query = mysqli_query($koneksi, "SELECT * FROM registrations WHERE email='$email' ");
 
 // validasi jika email sudah digunakan sebelumnya
 if (mysqli_num_rows($query) == 1) {
-    header("location:" . BASE_URL . "index.php?notif=email");
+    header("location:" . BASE_URL . "index.php?notif=email&email=$email&nama=$nama&alamat=$alamat");
 } else {
     mysqli_query($koneksi, "INSERT INTO registrations(nama,email,alamat)
                                         VALUES ('$nama','$email','$alamat')");
-    header("location:" . BASE_URL . "index.php?notif=berhasil&email=$email");
+    header("location:" . BASE_URL . "index.php?notif=berhasil&email=$email&nama=$nama&alamat=$alamat");
 }
